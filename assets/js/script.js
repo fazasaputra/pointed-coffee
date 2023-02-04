a2141ff1b197ce7d8f4cbdfce9f4618efd3a4b00
FazaSPT <evriliantiniandrian7@gmail.com>
2023-02-04 12:21:33 +0700

    pointed coffee website

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Toggle Class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+// While Hamburger menu clicked
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+};
+
+// While user click on outside sidebar , remove nav
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+})
